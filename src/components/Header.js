04e2e442b06d4afca53a26c275cde45db1c0fd5f
Crislande Pereira <cris_mg1994@hotmail.com>
2023-07-10 17:53:14 -0300

    feat: requisito 8

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import search from '../images/searchIcon.svg';
 import profile from '../images/profileIcon.svg';
 
 function Header({ showSearchIcon = true, title }) {
+  const history = useHistory();
+
+  const redirectToProfile = () => {
+    history.push('/profile');
+  };
+
   return (
     <div>
-      <img
-        src={ profile }
-        alt="profile icon"
-        data-testid="profile-top-btn"
-      />
-      {showSearchIcon && <img
+      <button onClick={ redirectToProfile }>
+        <img
+          src={ profile }
+          alt="profile icon"
+          data-testid="profile-top-btn"
+        />
+      </button>
+
+      {showSearchIcon
+      && <img
         src={ search }
         alt="search icon"
         data-testid="search-top-btn"
