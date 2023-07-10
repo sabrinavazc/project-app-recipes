6ad7c5affa4a178aeb4f8ec4f53be0c7b7793da1
Crislande Pereira <cris_mg1994@hotmail.com>
2023-07-10 17:13:26 -0300

    feat: requisito 7

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function DoneRecipes() {
   return (
     <div>
+      <Header title="Done Recipes" showSearchIcon={ false } />
       <h1>Done Recipes</h1>
     </div>
   );
--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Drinks() {
   return (
     <div>
+      <Header title="Drinks" />
       <h1>Drinks</h1>
     </div>
   );
