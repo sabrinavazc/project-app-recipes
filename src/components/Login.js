f0ed29b4d95fce53ad050dc550d504d3017ccce3
Danillo Shimofusa <shimofusa@hotmail.com>
2023-07-11 15:48:33 -0300

    feat login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 // initial commit
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import Header from './Header';
+import Footer from './Footer';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -11,42 +13,56 @@ function Login() {
     setPassword(value);
   };
 
-  const handleSubmit = () => {
-    localStorage.setItem('user', JSON.stringify({ email: user.email }));
-    useHistory.push('/meals');
+  const history = useHistory();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const salvar = { email };
+    localStorage.setItem('user', JSON.stringify(salvar));
+
+    history.push('/meals');
   };
 
-  const re = /\S+@\S+\.\S+/;
-  /* return re.test(email); */
+  /*   const re = /\S+@\S+\.\S+/;
+ return re.test(email);
   const six = 6;
-  const condition = re.test(email) && password.length >= six;
+  const condition = re.test(email) && password.length >= six; */
+
+  const validationEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(loginData.email);
+  const validationPassword = /.{7,}/.test(password.password);
+  const condition = !(validationEmail && validationPassword);
 
   return (
-    <form
-      onSubmit={ handleSubmit }
-    >
-      <input
-        type="email"
-        value={ email }
-        placeholder="Email"
-        data-testid="email-input"
-        onChange={ ({ target }) => setEmail(target.value) }
-      />
-      <input
-        type="password"
-        value={ password }
-        placeholder="Senha"
-        data-testid="password-input"
-        onChange={ handlePassword }
-      />
-      <button
-        type="submit"
-        disabled={ !condition }
-        data-testid="login-submit-btn"
+    <>
+      <form
+        onSubmit={ handleSubmit }
       >
-        Enter
-      </button>
-    </form>
+        <Header />
+        <input
+          type="email"
+          value={ email }
+          placeholder="Email"
+          data-testid="email-input"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+        <input
+          type="password"
+          value={ password }
+          placeholder="Senha"
+          data-testid="password-input"
+          onChange={ handlePassword }
+        />
+        <button
+          type="submit"
+          disabled={ !condition }
+          data-testid="login-submit-btn"
+        >
+          Enter
+        </button>
+      </form>
+      <Footer />
+
+    </>
   );
 }
 
