a19491845654b565f0c408d698ec64686d7aafa7
Crislande Pereira <cris_mg1994@hotmail.com>
2023-07-13 15:01:34 -0300

    feat: requisito 50 ao 56 tela de receitas favoritas

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,11 +1,140 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import copy from 'clipboard-copy';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import allFood from '../assets/allFood.svg';
+import food from '../assets/food.svg';
+import allDrinks from '../assets/allDrinks.svg';
+import share from '../images/shareIcon.svg';
+import favorite from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipes() {
+  const history = useHistory();
+  const [recipes, setRecipes] = useState([]);
+  const [fiteredRecipes, setFilteredRecipes] = useState([]);
+  const [linkCopied, setLinkCopied] = useState([]);
+
+  const getRecipesFavorite = () => {
+    const favoriteRecipesJSON = localStorage.getItem('favoriteRecipes') ?? '[]';
+    const favoriteRecipes = JSON.parse(favoriteRecipesJSON);
+    setRecipes(favoriteRecipes);
+    setFilteredRecipes(favoriteRecipes);
+  };
+
+  useEffect(() => {
+    getRecipesFavorite();
+  }, []);
+
+  const filterByAll = () => {
+    getRecipesFavorite();
+    setFilteredRecipes(recipes);
+  };
+
+  const filterByDrinks = () => {
+    const filterDrinks = recipes.filter((recipe) => recipe.type === 'drink');
+    setFilteredRecipes(filterDrinks);
+  };
+
+  const filterByMeals = () => {
+    const filterMeals = recipes.filter((recipe) => recipe.type === 'meal');
+    setFilteredRecipes(filterMeals);
+  };
+
+  const handleLinkCopy = async (id) => {
+    const { type } = recipes.filter((recipe) => recipe.id === id)[0];
+    const link = `${window.location.origin}/${type}s/${id}`;
+
+    const copyLink = async () => {
+      await copy(link);
+      const newLinkCopied = [...linkCopied, id];
+      setLinkCopied(newLinkCopied);
+    };
+
+    copyLink();
+  };
+
+  const redirectDetails = (id, type) => {
+    if (type === 'meal') {
+      history.push(`/meals/${id}`);
+    } else {
+      history.push(`/drinks/${id}`);
+    }
+  };
+
+  const removeFavorite = (id) => {
+    const favoriteRecipesJSON = localStorage.getItem('favoriteRecipes') ?? '[]';
+    const favoriteRecipes = JSON.parse(favoriteRecipesJSON);
+    const newFavoriteRecipes = favoriteRecipes.filter((recipe) => recipe.id !== id);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes ?? []));
+    getRecipesFavorite();
+  };
+
   return (
     <div>
       <Header title="Favorite Recipes" showSearchIcon={ false } />
       <h1>Favorite Recipes</h1>
+      <button onClick={ filterByAll } name="All" data-testid="filter-by-all-btn">
+        <img src={ allFood } alt="all" />
+      </button>
+      <button onClick={ filterByMeals } name="Meals" data-testid="filter-by-meal-btn">
+        <img src={ food } alt="food" />
+      </button>
+      <button onClick={ filterByDrinks } name="Drinks" data-testid="filter-by-drink-btn">
+        <img src={ allDrinks } alt="all" />
+      </button>
+
+      {fiteredRecipes?.map((recipe, index) => (
+        <div key={ index }>
+          <button
+            onClick={ () => redirectDetails(recipe.id, recipe.type) }
+            style={ {
+              border: 'none', background: 'none', padding: 0, cursor: 'pointer' } }
+          >
+            <img
+              src={ recipe.image }
+              data-testid={ `${index}-horizontal-image` }
+              alt="recipe"
+              style={ { width: '100px' } }
+            />
+          </button>
+          <p
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {recipe.type === 'meal'
+              ? `${recipe.nationality} - ${recipe.category}`
+              : recipe.alcoholicOrNot}
+          </p>
+          <button
+            onClick={ () => redirectDetails(recipe.id, recipe.type) }
+            style={ {
+              border: 'none', background: 'none', padding: 0, cursor: 'pointer' } }
+          >
+            <p data-testid={ `${index}-horizontal-name` }>
+              {recipe.type === 'meal' ? recipe.name : recipe.name}
+            </p>
+          </button>
+          <button
+            onClick={ () => handleLinkCopy(recipe.id) }
+          >
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ share }
+              alt="share"
+            />
+          </button>
+          <button
+            onClick={ () => removeFavorite(recipe.id) }
+          >
+            <img
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              src={ favorite }
+              alt="favorite"
+            />
+          </button>
+          {linkCopied.includes(recipe.id)
+          && <p style={ { background: 'yellow' } }>Link copied!</p>}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/tests/FavoriteRecipes.test.js
@@ -0,0 +1,205 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import copy from 'clipboard-copy';
+import { act } from 'react-dom/test-utils';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom/cjs/react-router-dom.min';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
+
+jest.mock('clipboard-copy', () => jest.fn().mockImplementation(() => Promise.resolve()));
+describe('Testando a página <FavoriteRecipes />', () => {
+  const name = 'Coddled pork with cider';
+  const recipe1 = '0-horizontal-name';
+  const recipe2 = '1-horizontal-name';
+  const mockData = [
+    {
+      id: '53037',
+      type: 'meal',
+      nationality: 'Irish',
+      category: 'Pork',
+      alcoholicOrNot: 'non-alcoholic',
+      name,
+      image: 'https://www.themealdb.com/images/media/meals/7vpsfp1608588991.jpg',
+    },
+    {
+      id: '52050',
+      type: 'drink',
+      nationality: 'Irish',
+      category: 'Shoots',
+      alcoholicOrNot: 'non-alcoholic',
+      name,
+      image: 'https://www.themealdb.com/images/media/meals/7vpsfp1608588991.jpg',
+    },
+  ];
+
+  const localStorageMock = (() => {
+    let store = {};
+
+    return {
+      getItem: jest.fn((key) => store[key] || null),
+      setItem: jest.fn((key, value) => {
+        store[key] = value.toString();
+      }),
+      removeItem: jest.fn((key) => {
+        delete store[key];
+      }),
+      clear: jest.fn(() => {
+        store = {};
+      }),
+    };
+  })();
+
+  const mockLocalStorage = () => {
+    Object.defineProperty(window, 'localStorage', {
+      value: localStorageMock,
+    });
+  };
+  test('1 - Testa se a página renderiza corretamente com todos os dados', () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    render(<FavoriteRecipes />, { wrapper: MemoryRouter });
+    console.log(localStorage.getItem('favoriteRecipes'));
+
+    const allButton = screen.getByTestId('filter-by-all-btn');
+    const mealsButton = screen.getByTestId('filter-by-meal-btn');
+    const drinksButton = screen.getByTestId('filter-by-drink-btn');
+    expect(allButton).toBeInTheDocument();
+    expect(mealsButton).toBeInTheDocument();
+    expect(drinksButton).toBeInTheDocument();
+
+    const recipe1Name = screen.getByTestId(recipe1);
+    const recipe2Name = screen.getByTestId(recipe2);
+    expect(recipe1Name.textContent).toBe(name);
+    expect(recipe2Name.textContent).toBe(name);
+  });
+
+  test('2 - Testa se a página renderiza corretamente com os dados filtrados por comida', () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    render(<FavoriteRecipes />, { wrapper: MemoryRouter });
+
+    const mealsButton = screen.getByTestId('filter-by-meal-btn');
+    fireEvent.click(mealsButton);
+
+    const recipe1Name = screen.getByTestId(recipe1);
+    expect(recipe1Name.textContent).toBe(name);
+  });
+
+  test('3 - Testa se a página renderiza corretamente com os dados filtrados por bebida', () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    render(<FavoriteRecipes />, { wrapper: MemoryRouter });
+
+    const drinksButton = screen.getByTestId('filter-by-drink-btn');
+    fireEvent.click(drinksButton);
+
+    const recipe1Name = screen.getByTestId(recipe1);
+    expect(recipe1Name.textContent).toBe(name);
+  });
+
+  test('4 - Testa se a página renderiza corretamente com os dados filtrados por todos', () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    render(<FavoriteRecipes />, { wrapper: MemoryRouter });
+
+    const allButton = screen.getByTestId('filter-by-all-btn');
+    fireEvent.click(allButton);
+
+    const recipe1Name = screen.getByTestId(recipe1);
+    const recipe2Name = screen.getByTestId(recipe2);
+    expect(recipe1Name.textContent).toBe(name);
+    expect(recipe2Name.textContent).toBe(name);
+  });
+
+  test('5 - Testa se quando clica em compartilhar, o link da página é copiado para o clipboard', async () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    delete window.location;
+    window.location = { origin: 'http://localhost:3000' };
+
+    render(<FavoriteRecipes />, { wrapper: MemoryRouter });
+    const recipe1ShareButton = screen.getByTestId('0-horizontal-share-btn');
+
+    await act(async () => {
+      fireEvent.click(recipe1ShareButton);
+    });
+
+    expect(copy).toHaveBeenCalledWith('http://localhost:3000/meals/53037');
+  });
+
+  test('6 - Testa se quando clica na imagem ou nome de uma receita de comida, redireciona para a pagina de detalhes da receita', () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={ history }>
+        <FavoriteRecipes />
+      </Router>,
+    );
+
+    const recipe1Image = screen.getByTestId('0-horizontal-image');
+    const recipe1Name = screen.getByTestId('0-horizontal-name');
+
+    fireEvent.click(recipe1Image);
+    expect(history.location.pathname).toBe('/meals/53037');
+
+    history.push('/favorite-recipes');
+
+    fireEvent.click(recipe1Name);
+    expect(history.location.pathname).toBe('/meals/53037');
+  });
+
+  test('7 - Testa se quando clica na imagem ou nome de uma receita de bebida, redireciona para a pagina de detalhes da receita', () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    const history = createMemoryHistory();
+
+    render(
+      <Router history={ history }>
+        <FavoriteRecipes />
+      </Router>,
+    );
+
+    const recipe1Image = screen.getByTestId('1-horizontal-image');
+    const recipe1Name = screen.getByTestId('1-horizontal-name');
+
+    fireEvent.click(recipe1Image);
+    expect(history.location.pathname).toBe('/drinks/52050');
+
+    history.push('/favorite-recipes');
+
+    fireEvent.click(recipe1Name);
+    expect(history.location.pathname).toBe('/drinks/52050');
+  });
+
+  test('8 - Testa se quando clica no icone de favorito ele desfavorita a receita e remove do localStorage', () => {
+    mockLocalStorage();
+    jest.spyOn(localStorage, 'getItem')
+      .mockImplementationOnce(() => JSON.stringify(mockData));
+
+    render(<FavoriteRecipes />, { wrapper: MemoryRouter });
+
+    const recipe1FavoriteButton = screen.getByTestId('0-horizontal-favorite-btn');
+    fireEvent.click(recipe1FavoriteButton);
+
+    const recipe1Name = screen.queryByTestId(recipe1);
+    expect(recipe1Name).not.toBeInTheDocument();
+    // expect(localStorage.getItem('favoriteRecipes')).toBe();
+  });
+});
