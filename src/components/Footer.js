dfe69d6e132f27a9bf4d6c43cfdf3c57acf5ceec
Crislande Pereira <cris_mg1994@hotmail.com>
2023-07-14 11:04:48 -0300

    feat: CSS Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,29 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import mealIcon from '../images/mealIcon.svg';
 import drinkIcon from '../images/drinkIcon.svg';
+import './Footer.css';
 
 function Footer() {
   return (
     <footer
+      className="footer-container"
       data-testid="footer"
-      style={ {
-        position: 'fixed',
-        bottom: 0,
-        width: '100%',
-      } }
     >
-      <Link to="/meals">
-        <img
-          src={ mealIcon }
-          alt="Comidas"
-          data-testid="meals-bottom-btn"
-        />
-      </Link>
       <Link to="/drinks">
         <img
+          className="drink-icon"
           data-testid="drinks-bottom-btn"
           src={ drinkIcon }
           alt="Bebidas"
+        />
+      </Link>
+      <Link to="/meals">
+        <img
+          className="meal-icon"
+          src={ mealIcon }
+          alt="Comidas"
+          data-testid="meals-bottom-btn"
         />
       </Link>
     </footer>
