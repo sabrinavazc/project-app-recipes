536863162970262b9c5034d25361bf235855ab6b
Crislande Pereira <cris_mg1994@hotmail.com>
2023-07-14 10:50:57 -0300

    feat: CSS Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import search from '../images/searchIcon.svg';
 import profile from '../images/profileIcon.svg';
 import SearchBar from './SearchBar';
+import logoHeader from '../assets/logo.svg';
+import './Header.css';
 
 function Header({ showSearchIcon = true, title }) {
   const history = useHistory();
@@ -18,26 +20,34 @@ function Header({ showSearchIcon = true, title }) {
   };
 
   return (
-    <div>
-      <button onClick={ redirectToProfile }>
-        <img
-          src={ profile }
-          alt="profile icon"
-          data-testid="profile-top-btn"
-        />
-      </button>
-      {showSearchBar && (<SearchBar isMeals={ title !== 'Drinks' } />)}
-      {showSearchIcon && (
-        <button onClick={ handleSearchBar }>
-          <img
-            src={ search }
-            alt="search icon"
-            data-testid="search-top-btn"
-          />
-        </button>
-      )}
+    <>
+      <div className="header-container">
+        <Link to="/meals">
+          <img src={ logoHeader } alt="logo" className="logo-header" />
+        </Link>
+
+        <div className="icons-header">
+          {showSearchIcon && (
+            <button className="btn-search" onClick={ handleSearchBar }>
+              <img
+                src={ search }
+                alt="search icon"
+                data-testid="search-top-btn"
+              />
+            </button>
+          )}
+          <button className="btn-profile" onClick={ redirectToProfile }>
+            <img
+              src={ profile }
+              alt="profile icon"
+              data-testid="profile-top-btn"
+            />
+          </button>
+        </div>
+      </div>
       <h1 data-testid="page-title">{title}</h1>
-    </div>
+      {showSearchBar && (<SearchBar isMeals={ title !== 'Drinks' } />)}
+    </>
   );
 }
 
